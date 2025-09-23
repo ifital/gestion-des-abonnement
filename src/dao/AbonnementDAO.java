653d0afa37138605d76140da457a727bdd680bb0
Latifi777 <abdelalilatifi3@gmail.com>
2025-09-23 14:45:15 +0100

    set up dao methodes for subscribionts and payments

---
+++ src/dao/AbonnementDAO.java
@@ -0,0 +1,18 @@
+package dao;
+
+import entity.abonnement.Abonnement;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AbonnementDAO {
+
+    void create(Abonnement abonnement);
+    Optional<Abonnement> findById(String id);
+    List<Abonnement> findAll();
+    void update(Abonnement abonnement);
+    void delete(String id);
+    List<Abonnement> findActivSubscribtions();
+    List<Abonnement> findByType(String type);
+
+}
---
+++ src/dao/PaiementDAO.java
@@ -0,0 +1,18 @@
+package dao;
+
+import entity.abonnement.Abonnement;
+import entity.paiement.Paiement;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PaiementDAO {
+    void create(Paiement paiement);
+    Optional<Paiement> findById(String id);
+    List<Paiement> findByAbonnement(String idAbonnement);
+    List<Paiement> findAll();
+    void update(Paiement paiement);
+    void delete(String id);
+    List<Paiement> findUnpaidAbonnment(String idAbonnement);
+    List<Paiement> findLastPaiment(int limit);
+}
