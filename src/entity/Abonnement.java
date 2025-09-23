4b7a223b4c60e8138b3761714aeda6f4c41d757a
Latifi777 <abdelalilatifi3@gmail.com>
2025-09-23 12:23:55 +0100

    generate abonnments classes

---
+++ src/entity/Abonnement.java
@@ -0,0 +1,87 @@
+package entity;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public abstract class Abonnement {
+    protected String id;
+    protected String nomService;
+    protected double montantMensuel;
+    protected LocalDate dateDebut;
+    protected LocalDate dateFin;
+    protected StatutAbonnement status;
+
+    public Abonnement (){
+        this.id = UUID.randomUUID().toString();
+        this.status = StatutAbonnement.ACTIF;
+    }
+
+    public Abonnement(String nomService, double montantMensuel, LocalDate dateDebut, LocalDate dateFin) {
+        this.nomService = nomService;
+        this.montantMensuel = montantMensuel;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNomService() {
+        return nomService;
+    }
+
+    public void setNomService(String nomService) {
+        this.nomService = nomService;
+    }
+
+    public double getMontantMensuel() {
+        return montantMensuel;
+    }
+
+    public void setMontantMensuel(double montantMensuel) {
+        this.montantMensuel = montantMensuel;
+    }
+
+    public LocalDate getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(LocalDate dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public LocalDate getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(LocalDate dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public StatutAbonnement getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatutAbonnement status) {
+        this.status = status;
+    }
+
+    public abstract String getTypeAbonnement();
+
+    @Override
+    public String toString() {
+        return "Abonnement{" +
+                "id='" + id + '\'' +
+                ", nomService='" + nomService + '\'' +
+                ", montantMensuel=" + montantMensuel +
+                ", dateDebut=" + dateDebut +
+                ", dateFin=" + dateFin +
+                ", status=" + status +
+                '}';
+    }
+}
---
+++ src/entity/AbonnementSansEngagement.java
@@ -0,0 +1,20 @@
+package entity;
+
+import java.time.LocalDate;
+
+public class AbonnementSansEngagement extends Abonnement {
+
+    public AbonnementSansEngagement() {
+        super();
+    }
+
+    public AbonnementSansEngagement(String nomService, double montantMensuel,
+                                    LocalDate dateDebut, LocalDate dateFin) {
+        super(nomService, montantMensuel, dateDebut, dateFin);
+    }
+
+    @Override
+    public String getTypeAbonnement() {
+        return "SANS_ENGAGEMENT";
+    }
+}
